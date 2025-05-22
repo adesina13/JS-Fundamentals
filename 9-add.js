0e316df5f4e8f99b5a3686df0e0142620e31027c
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-05-22 21:56:32 +0100

    task 9

--- 9-add.js
+++ 9-add.js
@@ -3,7 +3,7 @@ const {argv} = require('node:process')
 if (argv[2] && argv[3]){
     let num1 = Number(argv[2])
     let num2 = Number(argv[3])
-    if (num1 != NaN && num1 > 0 && num2 != NaN && num2 > 0){
+    if (num1 != NaN  && num2 != NaN ){
         let result = num1 + num2
         console.log(result)
     }else{
