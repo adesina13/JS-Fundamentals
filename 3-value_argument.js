7614494b0bb04b5372d72b3052114c765a4745f6
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-05-22 17:10:59 +0100

    task 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,10 @@
 const {argv} = require('node:process')
 
 if (argv[2]){
-    argv.forEach(arg => {
-        console.log(arg)
+    argv.forEach((arg, index) => {
+        if (index >= 2){
+            console.log(arg)
+        }
     })
 }else{
     console.log("No argument")
