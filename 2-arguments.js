baa406021f3de588b9b00bd174c40c29e175c17e
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-05-22 17:00:06 +0100

    task 2

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,6 @@
 const { argv } = require('node:process')
 
-if (argv.length < 2){
+if (argv.length <= 2){
     console.log('No argument')
 }else if(argv.length == 3){
     console.log('Argument found')
